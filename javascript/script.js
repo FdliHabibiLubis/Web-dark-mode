abdb682a47eb90be1aebf5295e47999e4652dcaa
Fdli Habibi Lubis <habibilubisf@gmail.com>
2026-04-14 19:58:36 +0700

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,16 @@
 let body = document.body;
+const darkLimit = 5; // limit klik nya
+let darkCounter = 0; // Banyak klik nya
+let darkRemainingLeft; // sisa klik nya
 
 function darkMode() {
-    console.log("hello");
-    body.classList.toggle("dark");
+  if (darkRemainingLeft == 1) {
+    info.textContent = "Jatah Fitur Dark Mode sudah habis";
+    return;
+  }
+  darkCounter += 1;
+  darkRemainingLeft = darkLimit - darkCounter;
+
+  info.textContent = `Dark mode diklik ${darkCounter} tersisa ${darkRemainingLeft}`
+  body.classList.toggle("dark");
 }
